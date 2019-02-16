ad57aa0888935e3a9cad50ab09582918a2d6b5e1
chou0728 <dean34520@gmail.com>
2019-02-16 20:02:38 +0800

    add deploy setting

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-// import Form from './components/Form';
-import FormWithItems from './components/FormWithItems';
+import Form from './components/Form';
+// import FormWithItems from './components/FormWithItems';
 import './App.css';
 
 class App extends Component {
@@ -14,7 +14,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <FormWithItems />
+        <Form />
       </div>
     );
   }
